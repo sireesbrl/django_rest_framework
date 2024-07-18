f32f9c602812253750279132c134a1a92ba351cf
Sirees <sireesbrl@gmail.com>
2024-07-18 16:11:56 +0545

    django_rest_framework tutorial6-using routers

--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,10 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework import permissions, renderers, viewsets
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, action
-from rest_framework.reverse import reverse
+from rest_framework.decorators import action
 from rest_framework.response import Response
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
